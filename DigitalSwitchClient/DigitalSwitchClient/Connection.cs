aa3a90baa37eed9db11e4f8b5e6989b934d7aa4f
Bruno David da Silva Almeida <bruno.almeida@autotrac.com.br>
2018-06-20 18:00:41 -0300

    Classe de conexao criada(ainda falta implementar)

---
+++ DigitalSwitchClient/DigitalSwitchClient/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+
+namespace DigitalSwitchClient
+{
+    public class Connection
+    {
+        private const string URL = "";
+        private const string OPERATION_STATUS = "";
+        private const string OPERATION_SET_STATUS = "";
+
+        public void GetStatus(string lamp)
+        {
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync(URL);
+        }
+    }
+}
